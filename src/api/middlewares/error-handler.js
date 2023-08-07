df203716e7144925b50b1e138ef8f33f357a5cda
Abdur Rakib <abdurrakib961@gmail.com>
2023-08-07 18:04:42 +0600

    feat: handle create product body validation error

--- src/api/middlewares/error-handler.js
+++ src/api/middlewares/error-handler.js
@@ -2,8 +2,14 @@ const { StatusCodes } = require("http-status-codes");
 const generateResponse = require("../helpers/generateResponse");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  const message = err.message || "Something went wrong try again later";
+  let statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = err.message || "Something went wrong try again later";
+  if (err.name === "ValidationError") {
+    message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    statusCode = 400;
+  }
 
   return res.status(statusCode).json(generateResponse(false, null, message));
 };
